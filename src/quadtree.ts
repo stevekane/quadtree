0ceecb214205e7ca2a3a91af1fe3856e77e8994f
Steve Kane <kanesteven@gmail.com>
2017-03-15 13:25:52 -0500

    lots of quad tree experimenting

--- src/quadtree.ts
+++ src/quadtree.ts
@@ -78,34 +78,15 @@ function insert<T> ( q: IQuad<T>, l: ILeaf<T> ) {
   }
 }
 
-function sortProximity ( [ x, y ]: V2<number> ) {
-  return function <T>( q1: QT<T>, q2: QT<T> ): number {
-    if ( q1 == null ) return 1
-    if ( q1.leaf )    return 1
-    if ( q2 == null ) return -1
-    if ( q2.leaf )    return -1
-   
-    var [ q1x, q1y ] = q1.position
-    var [ q2x, q2y ] = q2.position
-    var d1 = abs(q1x - x) + abs(q1y - y)
-    var d2 = abs(q2x - x) + abs(q2y - y)
-    
-    return d1 > d2 ? 1 : -1
-  }
-}
-
 function manhattan ( p0: V2<number>, p1: V2<number> ): number {
   return abs(p1[0] - p0[0]) + abs(p1[1] - p0[1])
 }
 
 function L2 ( p0: V2<number>, p1: V2<number> ): number {
-  var dx = p1[0] - p0[0]
-  var dy = p1[1] - p0[1]
-
-  return sqrt(pow(dx, 2) + pow(dy, 2))
+  return sqrt(pow(p1[0] - p0[0], 2) + pow(p1[1] - p0[1], 2))
 }
 
-function nearestArray <T> ( nodes: ILeaf<T>[], dfn: DistanceFn, p: V2<number> ): null | ILeaf<T> {
+function nearestArray <T> ( nodes: ILeaf<T>[], dfn: DistanceFn, r: number, p: V2<number> ): null | ILeaf<T> {
   var found = null
 
   for ( var i = 0, d; i < nodes.length; i++ ) {
@@ -115,22 +96,61 @@ function nearestArray <T> ( nodes: ILeaf<T>[], dfn: DistanceFn, p: V2<number> ):
   return found
 }
 
+function minDistToQuad <T> ( q: IQuad<T>, p: V2<number> ): number {
+  var x = p[0]
+  var y = p[1]
+  var halfW = q.dimension[0] / 2
+  var halfH = q.dimension[1] / 2
+  var cx = q.position[0]
+  var cy = q.position[1]
+  var d1x = abs(x - cx + halfW)
+  var d2x = abs(x - cx - halfW)
+  var d1y = abs(y - cy + halfH)
+  var d2y = abs(y - cy - halfH)
+
+  return min(d1x, d2x, d1y, d2y)
+}
+
+// TODO: JUST FOR TESTING
+var recursion = 0
+
 function nearest <T> ( q: QT<T>, dfn: DistanceFn, r: number, p: V2<number> ): ILeaf<T> | null {
+  function proximity <T>( q1: QT<T>, q2: QT<T> ): number {
+    if ( q1 == null || q1.leaf ) return 1
+    if ( q2 == null || q2.leaf ) return -1
+   
+    return dfn(q1.position, p) > dfn(q2.position, p) ? 1 : -1
+  }
+
   function search ( w: ILeaf<T> | null, c: QT<T> ): ILeaf<T> | null {
-    const n = nearest(c, dfn, r, p) 
+    if ( c == null ) return w
+    if ( c.leaf ) {
+      if ( w == null ) return c 
+      else             return dfn(c.position, p) < r ? c : w
+    }
 
-    if ( w == null ) return n
-    if ( n == null ) return w
+    const md = minDistToQuad(c, p)
+    const wd = w == null ? r : dfn(w.position, p)
 
-    return dfn(n.position, p) < dfn(w.position, p) ? n : w
+    if ( md > wd ) return w
+    
+    recursion++
+    const n = nearest(c, dfn, wd, p) 
+
+    if      ( w == null ) return n
+    else if ( n == null ) return w
+    else                  return dfn(n.position, p) < wd ? n : w
   }
 
   if      ( q == null ) return null
   else if ( q.leaf )    return dfn(q.position, p) < r ? q : null
-  else                  return q.children.reduce(search, null as null | ILeaf<T>) as null | ILeaf<T>
+  else                  return q.children
+                                .slice(0)
+                                .sort(proximity)
+                                .reduce(search, null as null | ILeaf<T>) as null | ILeaf<T>
 }
 
-const MAX_NODES = pow(2, 3)
+const MAX_NODES = pow(2, 14)
 const r = Quad([ 0, 0 ], [ 1, 1 ])
 const target: V2<number> = [ 0.5, 0.5 ]
 const nodes = []
@@ -161,22 +181,22 @@ for ( var i = 0, n; i < MAX_NODES; i++ ) {
 // 
 // console.log(`FOUND SAME: ${ closest_array === closest_qt }`)
 
-var problemLeaves: ILeaf<any>[] = [ 
-  { id: 1, leaf: true, position: [ 0.20959098742382576, -0.6049647185031937 ] },
-  { id: 2, leaf: true, position: [ -0.15272271894029465, -0.4099223518846471 ] },
-  { id: 3, leaf: true, position: [ -0.20917872072493626, -0.8297414166119115 ] },
-  { id: 4, leaf: true, position: [ -0.046198461922013934, -0.687954309298973 ] },
-  { id: 5, leaf: true, position: [ 0.5686020477613236, -0.8017516705096708 ] },
-  { id: 6, leaf: true, position: [ -0.6986326028896657, 0.6377256037676262 ] },
-  { id: 7, leaf: true, position: [ -0.6763076967429464, -0.18301920072377742 ] },
-  { id: 8, leaf: true, position: [ -0.4952690427277786, -0.9748926589056364 ] }
-]
-
-const rp = Quad([ 0, 0 ], [ 1, 1 ])
-
-for ( var i = 0; i < problemLeaves.length; i++ ) {
-  insert(rp, problemLeaves[i])
-}
+// var problemLeaves: ILeaf<any>[] = [ 
+//   { id: 1, leaf: true, position: [ 0.20959098742382576, -0.6049647185031937 ] },
+//   { id: 2, leaf: true, position: [ -0.15272271894029465, -0.4099223518846471 ] },
+//   { id: 3, leaf: true, position: [ -0.20917872072493626, -0.8297414166119115 ] },
+//   { id: 4, leaf: true, position: [ -0.046198461922013934, -0.687954309298973 ] },
+//   { id: 5, leaf: true, position: [ 0.5686020477613236, -0.8017516705096708 ] },
+//   { id: 6, leaf: true, position: [ -0.6986326028896657, 0.6377256037676262 ] },
+//   { id: 7, leaf: true, position: [ -0.6763076967429464, -0.18301920072377742 ] },
+//   { id: 8, leaf: true, position: [ -0.4952690427277786, -0.9748926589056364 ] }
+// ]
+// 
+// const rp = Quad([ 0, 0 ], [ 1, 1 ])
+// 
+// for ( var i = 0; i < problemLeaves.length; i++ ) {
+//   insert(rp, problemLeaves[i])
+// }
 
 // const pmarray = nearestArray(problemLeaves, manhattan, target)
 // const pmqt = nearest(rp, manhattan, target)
@@ -186,9 +206,10 @@ for ( var i = 0; i < problemLeaves.length; i++ ) {
 //   console.log(pmqt, manhattan(pmqt.position, target))
 // }
 
-const pdarray = nearestArray(problemLeaves, L2, target)
-const pdqt = nearest(rp, L2, Infinity, target)
+const pdarray = nearestArray(nodes, L2, Infinity, target)
+const pdqt = nearest(r, L2, Infinity, target)
 if ( pdarray != null && pdqt != null ) {
+  console.log(recursion)
   console.log(pdarray, L2(pdarray.position, target))
   console.log(pdqt, L2(pdqt.position, target))
 }
