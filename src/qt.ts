ba809c3c5247e1250fc0ea05322c111c4478f914
Steve Kane <kanesteven@gmail.com>
2017-03-16 14:01:02 -0500

    playing code-golf with quadtree to warmup the brain :/

--- src/qt.ts
+++
@@ -1,7 +0,0 @@
-type V2 = [ number, number ]
-
-interface ILeaf<T> { leaf: true, position: V2 }
-interface IRegion<T> { leaf: false, children: QT<T>[] }
-type QT<T> = ILeaf<T> | IRegion<T>
-
-function insert ( qt: QT<T>, 
